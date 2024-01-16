eea1795a5c045fee0392ed177cbd5adddc321eb9
JosFer720 <119770216+JosFer720@users.noreply.github.com>
2024-01-16 16:06:29 -0600

    Inicio menu de ingreso

--- Principal.java
+++ Principal.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Radio radio = new Radio(true, "AM", 530.0, 87.9, new double[12], new double[12]);
+
+        int option = -1;
+
+        while (option != 0) {
+            System.out.println("====Radio====");
+            System.out.println("1. Encender o apagar");
+            System.out.println("2. Cambiar frecuencia");
+            System.out.println("3. Guardar estacion");
+            System.out.println("4. Seleccionar estacion guardada ");
+            System.out.println("5. Cambiar estacion");
+            System.out.println("6. Salir");
+            System.out.println("Ingrese su opcion");
+
+            option = scanner.nextInt();
+
+            switch (option) {
+
+            }
+        }
     }
 }
