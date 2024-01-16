e45fa31cbf01cd9cf2525963fca0d80ab83bd618
JosFer720 <119770216+JosFer720@users.noreply.github.com>
2024-01-16 17:37:13 -0600

    Prueba cambio de estacion

--- Pruebas.java
+++ Pruebas.java
@@ -1,3 +1,13 @@
+
+
 public class Pruebas{
-    
+    @Test
+    public void testNextStation() {
+        Radio radio = new Radio(true, "AM", 530.0, 87.9, new double[12], new double[12]);
+
+        double initialStation = radio.nextStation();
+        double secondStation = radio.nextStation();
+
+        assertTrue(initialStation < secondStation);
+    }
 }
