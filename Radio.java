4a2f1c7e544072df1fab60456dc2c4d96710a72d
JosFer720 <119770216+JosFer720@users.noreply.github.com>
2024-01-15 19:31:42 -0600

    Creacion de metodos dentro de la clase Radio

--- Radio.java
+++ Radio.java
@@ -1,30 +1,48 @@
-// public class Radio implements IRadio {
+public class Radio implements IRadio {
+    private boolean ON;
+    private String frequency;
+    private double station;
+    private double[] buttonListAM;
+    private double[] buttonListFM;
+
+    public Radio(boolean ON, String frequency, double station, double[] buttonListAM, double[] buttonListFM){
+        this.ON = true;
+        this.frequency = "AM";
+        this.station = 0;
+        this.buttonListAM = new double[12];
+        this.buttonListFM = new double[12];
+    }
     
-//     public void saveStation(int buttonId, double station){
+     public void saveStation(int buttonId, double station){
+     }
 
-//     }
-
-//     public boolean isAm() {
-
-//     }
+     public boolean isAm() {
+        if (this.frequency == "AM"){
+            return true;
+        }
+        else {
+            return false;
+        }
+     }
     
-//     public boolean isOn() {
+    public boolean isOn() {
+        return this.ON;
+    }
 
-//     }
+    public double selectStation(int buttonId) {
+        return this.station;
+    }
 
-//     public double selectStation(int buttonId) {
+    public void switchOnOff() {
 
-//     }
+    }
 
-//     public void switchOnOff() {
+    public void switchAMFM() {
+        
+    }
 
-//     }
+    public double nextStation() {
+        return this.buttonListAM[2]+this.buttonListFM[12];
+    }
 
-//     public void switchAMFM() {
-
-//     }
-
-//     public double nextStation() {
-
-//     }
-// }
+ }
